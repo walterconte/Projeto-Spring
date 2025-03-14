dfc037c1895a48142070d6f593a2503c09cc0b3b
vagnerantunes <vagner.antunes94@gmail.com>
2025-03-14 14:13:59 -0300

    atualizando projeto

---
+++ src/main/java/org/example/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package org.example.repositories;
+
+import org.example.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
---
+++ src/main/java/org/example/resources/UsuarioResource.java
@@ -0,0 +1,4 @@
+package org.example.resources;
+
+public class UsuarioResource {
+}

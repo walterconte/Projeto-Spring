93e1ad0927cc532f95422addd570597f28562b7e
vagnerantunes <vagner.antunes94@gmail.com>
2025-03-14 15:52:09 -0300

    atualizando projeto

--- src/main/java/org/example/resources/UsuarioResource.java
+++ src/main/java/org/example/resources/UsuarioResource.java
@@ -1,4 +1,54 @@
 package org.example.resources;
 
+import org.example.entities.Usuario;
+import org.example.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/usuarios")
 public class UsuarioResource {
+
+    @Autowired
+    private UsuarioService service;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll() {
+        List<Usuario> usuario = service.findAll();
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+        Optional<Usuario> usuario = service.findById(id);
+        return usuario.map(ResponseEntity::ok).orElseGet(()
+                -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> insert(@RequestBody Usuario usuario) {
+        Usuario created = service.insert(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id,
+                                    @RequestBody Usuario usuario) {
+        if (service.update(id, usuario)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/org/example/services/UsuarioService.java
+++ src/main/java/org/example/services/UsuarioService.java
@@ -1,4 +1,48 @@
 package org.example.services;
 
+import org.example.entities.Usuario;
+import org.example.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    //Listar todos usuários
+    public List<Usuario> findAll() {
+        return repository.findAll();
+    }
+
+    //Listar um unico usuário por id
+    public Optional<Usuario> findById(Long id) {
+
+        return repository.findById(id);
+    }
+
+    public Usuario insert(Usuario usuario) {
+
+        return repository.save(usuario);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public boolean update(Long id, Usuario usuario) {
+        Optional<Usuario> optional = repository.findById(id);
+        if (optional.isPresent()) {
+            Usuario usuario1 = optional.get();
+            usuario1.setNome(usuario.getNome());
+            usuario1.setIdade(usuario.getIdade());
+            repository.save(usuario1);
+            return true;
+        }
+        return false;
+    }
 }

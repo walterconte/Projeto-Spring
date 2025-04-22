45656dad9dab987fddae1bec31e13af07ce1011e
walterconte <waltercontemartimdelima@gmail.com>
2025-04-22 15:54:04 -0300

    atualizado

--- src/main/java/org/example/resources/FornecedorResource.java
+++ src/main/java/org/example/resources/FornecedorResource.java
@@ -1,4 +1,15 @@
 package org.example.resources;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping(value ="/fornecedores")
 public class FornecedorResource {
+
+
 }

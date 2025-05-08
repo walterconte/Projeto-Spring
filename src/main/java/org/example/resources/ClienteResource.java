fae2e91bc6b65598917c4f3b7f52935f59262704
walterconte <waltercontemartimdelima@gmail.com>
2025-05-08 16:59:51 -0300

    Cliente Service e Resource adicionado

--- src/main/java/org/example/resources/ClienteResource.java
+++ src/main/java/org/example/resources/ClienteResource.java
@@ -1,4 +1,53 @@
 package org.example.resources;
 
+import org.example.entities.Cliente;
+import org.example.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAll() {
+        List<Cliente> funcoes = clienteService.getAll();
+        return ResponseEntity.ok(funcoes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable Long id) {
+        Cliente obj = clienteService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> insert(@RequestBody Cliente cliente) {
+        Cliente createdCliente = clienteService.insert(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Cliente cliente) {
+        if (clienteService.update(id, cliente)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/org/example/services/ClienteService.java
+++ src/main/java/org/example/services/ClienteService.java
@@ -1,4 +1,47 @@
 package org.example.services;
 
+import org.example.entities.Cliente;
+import org.example.repositories.ClienteRepository;
+import org.example.services.exeptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    public List<Cliente> getAll(){
+        return repository.findAll();
+    }
+
+    public Cliente findById(Long id){
+        Optional<Cliente> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Cliente insert(Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    public boolean update(Long id, Cliente cliente){
+        Optional<Cliente> optionalCliente = repository.findById(id) ;
+        if (optionalCliente.isPresent()){
+            Cliente clienteSistema = optionalCliente.get();
+            clienteSistema.setCliNome(cliente.getCliNome());
+            clienteSistema.setCliCpf(cliente.getCliCpf());
+            clienteSistema.setCliEmail(cliente.getCliEmail());
+            clienteSistema.setCliTelefone(cliente.getCliTelefone());
+            repository.save(clienteSistema);
+            return true;
+        }
+        return false;
+    }
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
